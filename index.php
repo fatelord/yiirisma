6957a76c50488ad6a8c852ad651ae6df5d7e5244
fatelord <barsamms@gmail.com>
2014-05-26 17:42:26 +0000

    Update index.php
    Added IP filters to switch trace and log details based on the environment
    Assumes that localhost and 127.0.0.1 are development environments

--- index.php
+++ index.php
@@ -8,8 +8,17 @@
 $yii=dirname(__FILE__).'/../framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 
+//Added IP filters to switch trace and log details based on the environment
+//Assumes that localhost and 127.0.0.1 are development environments
+if ($machine_ip != '127.0.0.1') {
+    /* production mode */
+} else {
+    defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 1);
+    defined('YII_DEBUG') or define('YII_DEBUG', true);
+}
+
 // remove the following line when in production mode
-defined('YII_DEBUG') or define('YII_DEBUG',true);
+//defined('YII_DEBUG') or define('YII_DEBUG',true);
 
 require_once($yii);
 Yii::createWebApplication($config)->run();
